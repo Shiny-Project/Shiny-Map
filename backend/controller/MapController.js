0d1d54282a0c787fea436257a7e97e6303e790ee
Eridanus Sora <haolinaihui@Gmail.com>
2018-08-12 19:53:44 +0800

    add docs

--- backend/controller/MapController.js
+++ backend/controller/MapController.js
@@ -4,14 +4,49 @@ class MapController {
     constructor() {
 
     }
+    /**
+     * 中国大陆地区市级指定高亮
+     * @param {*} request 
+     * @param {*} response 
+     */
     async highlight(request, response) {
         if (!global.isBrowserLoaded) {
-            response.json(APIResponse.error('try_again_later', '人家还没准备好'));
+            return response.status(500).json(APIResponse.error('try_again_later', '人家还没准备好'));
         }
-        const page = global.page;
-        const needHighlight = ["温州市"];
+
+        let needHighlight;
+        // 参数验证
         try {
-            await page.evaluate((needHighlight) => {
+            needHighlight = JSON.parse(request.query.cities);
+        } catch (e) {
+            return response.status(400).json(APIResponse.error('bad_json', '无法解析 JSON'));
+        }
+        if (!Array.isArray(needHighlight)) {
+            return response.status(400).json(APIResponse.error('need_array', 'cities 需要为数组'));
+        }
+
+        const center = request.query.center;
+
+        if (!center) {
+            return response.status(400).json(APIResponse.error('missing_parameters', '缺少 center 参数'));
+        }
+        
+        const provinceData = require('../definition/china_provinces');
+        
+        if (!Object.keys(provinceData).includes(center)) {
+            return response.status(400).json(APIResponse.error('missing_parameters', '不支持的省份'));
+        }
+
+        if (["云南省", "西藏自治区"].includes(center)) {
+            return response.status(451).json(APIResponse.error('missing_parameters', '不支持的省份'));
+        }
+
+        const page = global.page;
+
+        try {
+            await page.evaluate((needHighlight, provinceData, center) => {
+                map.getView().setCenter(ol.proj.fromLonLat([provinceData[center].longitude, provinceData[center].latitude]));
+                map.getView().setZoom(provinceData[center].zoom);
                 map.getLayers().getArray()[0].getSource().forEachFeature(feature => {
                     if (needHighlight.includes(feature.getProperties()['NL_NAME_2'])) {
                         feature.setStyle(new ol.style.Style({
@@ -25,7 +60,7 @@ class MapController {
                         }))
                     }
                 });
-            }, needHighlight)
+            }, needHighlight, provinceData, center)
         } catch (e) {
             console.log('Navigation failed: ' + e.message);
         }
@@ -33,6 +68,21 @@ class MapController {
         await page.screenshot({
             path: "test.png"
         });
+
+        await page.evaluate(() => {
+            // Clean
+            map.getLayers().getArray()[0].getSource().forEachFeature(feature => {
+                feature.setStyle(new ol.style.Style({
+                    fill: new ol.style.Fill({
+                        color: '#444444'
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: '#aaa',
+                        width: 1
+                    }),
+                }))
+            });
+        })
 
         response.json(APIResponse.success({
             status: "success"
--- backend/definition/china_provinces.js
+++ backend/definition/china_provinces.js
@@ -2,11 +2,36 @@ module.exports = {
     "海南省": {
         "longitude": 109.9497,
         "latitude": 19.5664,
-        "zoom": 4
+        "zoom": 8
     },
     "广东省": {
         "longitude": 113.7633,
-        "latitude": 23.3790,
-        "zoom": 4
+        "latitude": 22.9,
+        "zoom": 7.5
+    },
+    "四川省": {
+        "longitude": 102.8055,
+        "latitude": 30.2638,
+        "zoom": 6.7
+    },
+    // "云南省": {
+    //     "longitude": 101.3431,
+    //     "latitude": 24.4753,
+    //     "zoom": 6.7
+    // }
+    "浙江省": {
+        "longitude": 119.7889,
+        "latitude": 29.1416,
+        "zoom": 8.3
+    },
+    "上海市": {
+        "longitude": 121.4737,
+        "latitude": 31.2304,
+        "zoom": 9
+    },
+    "江苏省": {
+        "longitude": 119.7889,
+        "latitude": 33.0402,
+        "zoom": 7.5
     }
 }
