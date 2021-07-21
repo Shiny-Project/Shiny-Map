0ad7ad3e852c5daff149512207ad5b942aa3b38a
Eridanus Sora <sora@sound.moe>
2021-07-22 00:12:01 +0800

    add sentry

--- backend/index.js
+++ backend/index.js
@@ -1,64 +1,95 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const MapController = require('./controller/MapController');
+const express = require("express");
+require("dotenv").config();
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+const path = require("path");
+const bodyParser = require("body-parser");
+const MapController = require("./controller/MapController");
 
 const app = express();
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // Load Browser
 global.isBrowserLoaded = false;
 global.isBrowserBusy = false;
 
-const puppeteer = require('puppeteer');
-puppeteer.launch({
-    args: ['--no-sandbox']
-}).then(async browser => {
-    // Earthquake Page
-    const page = await browser.newPage();
-    page.setViewport({
-        width: 1920,
-        height: 1080
-    });
-    page.on('console', msg => {
-        for (let i = 0; i < msg.args().length; ++i)
-          console.log(`${i}: ${msg.args()[i]}`);
-      });
-    await page.goto(`http://localhost:3000/template/index.html`, {
-        waitUntil: 'networkidle0'
-    });
+const puppeteer = require("puppeteer");
+puppeteer
+    .launch({
+        args: ["--no-sandbox"],
+    })
+    .then(async (browser) => {
+        // Earthquake Page
+        const page = await browser.newPage();
+        page.setViewport({
+            width: 1920,
+            height: 1080,
+        });
+        page.on("console", (msg) => {
+            for (let i = 0; i < msg.args().length; ++i)
+                console.log(`${i}: ${msg.args()[i]}`);
+        });
+        await page.goto(`http://localhost:3000/template/index.html`, {
+            waitUntil: "networkidle0",
+        });
 
-    // Tsunami Page
-    const tsunamiPage = await browser.newPage();
-    tsunamiPage.setViewport({
-        width: 1920,
-        height: 1080
-    });
-    tsunamiPage.on('console', msg => {
-        for (let i = 0; i < msg.args().length; ++i)
-          console.log(`${i}: ${msg.args()[i]}`);
-      });
-    await tsunamiPage.goto(`http://localhost:3000/template/index.html`, {
-        waitUntil: 'networkidle0'
-    });
+        // Tsunami Page
+        const tsunamiPage = await browser.newPage();
+        tsunamiPage.setViewport({
+            width: 1920,
+            height: 1080,
+        });
+        tsunamiPage.on("console", (msg) => {
+            for (let i = 0; i < msg.args().length; ++i)
+                console.log(`${i}: ${msg.args()[i]}`);
+        });
+        await tsunamiPage.goto(`http://localhost:3000/template/index.html`, {
+            waitUntil: "networkidle0",
+        });
 
-    global.isBrowserLoaded = true;
-    global.browser = browser;
-    global.page = page;
-    global.tsunamiPage = tsunamiPage;
-    console.log('Browser Loaded.');
-})
+        global.isBrowserLoaded = true;
+        global.browser = browser;
+        global.page = page;
+        global.tsunamiPage = tsunamiPage;
+        console.log("Browser Loaded.");
+    });
 
 // 静态文件服务目录
-app.use('/data', express.static(path.join(__dirname, '../data')));
-app.use('/template', express.static(path.join(__dirname, '../dist')));
+app.use("/data", express.static(path.join(__dirname, "../data")));
+app.use("/template", express.static(path.join(__dirname, "../dist")));
 
-app.use('/map/highlight', MapController.highlight);
-app.use('/map/shindo_early_report', MapController.shindoEarlyReport);
-app.use('/map/shindo_report', MapController.shindoReport);
-app.use('/map/tsunami_warning', MapController.tsunamiWarning);
+app.use("/map/highlight", MapController.highlight);
+app.use("/map/shindo_early_report", MapController.shindoEarlyReport);
+app.use("/map/shindo_report", MapController.shindoReport);
+app.use("/map/tsunami_warning", MapController.tsunamiWarning);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
