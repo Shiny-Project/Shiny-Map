66fe02d6e32889b137693e55421eb8515123f09f
Eridanus Sora <sora@sound.moe>
2022-01-01 00:05:28 +0800

    2021->2022

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,11 +1,28 @@
-module.exports = {
-    encodeParameters: (parameters) => {
-        const base64 = require('base64-utf8');
+class Utils {
+    static encodeParameters(parameters) {
+        const base64 = require("base64-utf8");
         return base64.encode(JSON.stringify(parameters));
-    },
-    getTimestamp: () => {
+    }
+    static getTimestamp() {
         const time = new Date();
-        return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}-${time.getHours()}${time.getMinutes()}${time.getSeconds()}${time.getMilliseconds()}`;
-    },
-    sleep: (delay) => new Promise(resolve => setTimeout(resolve, delay))
+        return `${time.getFullYear()}-${
+            time.getMonth() + 1
+        }-${time.getDate()}-${time.getHours()}${time.getMinutes()}${time.getSeconds()}${time.getMilliseconds()}`;
+    }
+    static sleep() {
+        return new Promise((resolve) => setTimeout(resolve, delay));
+    }
+    static lockBrowser() {
+        global.isBrowserBusy = true;
+    }
+    static unlockBrowser() {
+        global.isBrowserBusy = false;
+    }
+    static async waitBrowserAvailable() {
+        while (global.isBrowserBusy) {
+            await Utils.sleep(200);
+        }
+    }
 }
+
+module.exports = new Utils();
--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-import 'ol/ol.css';
+import "ol/ol.css";
 
 const defaultStyle = new ol.style.Style({
     fill: new ol.style.Fill({
-        color: '#F5F5F5'
+        color: "#F5F5F5",
     }),
     stroke: new ol.style.Stroke({
-        color: '#aaa',
-        width: 1
+        color: "#aaa",
+        width: 1,
     }),
 });
 
 const ChinaMapVectorSource = new ol.source.Vector({
-    url: '../data/gadm36_CHN_2.json',
+    url: "../data/gadm36_CHN_2.json",
     format: new ol.format.GeoJSON(),
 });
 
 const JapanMapVectorSource = new ol.source.Vector({
-    url: '../data/gadm36_JPN_2.json',
+    url: "../data/gadm36_JPN_2.json",
     format: new ol.format.GeoJSON(),
 });
 
 const NorthKoreaVectorSource = new ol.source.Vector({
-    url: '../data/gadm36_PRK_0.json',
+    url: "../data/gadm36_PRK_0.json",
     format: new ol.format.GeoJSON(),
 });
 
 const SouthKoreaVectorSource = new ol.source.Vector({
-    url: '../data/gadm36_KOR_0.json',
+    url: "../data/gadm36_KOR_0.json",
     format: new ol.format.GeoJSON(),
 });
 
 const TaiwanVectorSource = new ol.source.Vector({
-    url: '../data/gadm36_TWN_0.json',
+    url: "../data/gadm36_TWN_0.json",
     format: new ol.format.GeoJSON(),
 });
 
 const JMAEQVectorSource = new ol.source.Vector({
-    url: '../data/jma_eq_area.json',
+    url: "../data/jma_eq_area.json",
     format: new ol.format.GeoJSON(),
 });
 
@@ -44,41 +44,41 @@ const JMAEQVectorSource = new ol.source.Vector({
 
 window.JapanDetailedMapLayer = new ol.layer.Vector({
     source: JapanMapVectorSource,
-    style: defaultStyle
+    style: defaultStyle,
 });
 
-window.JapanDetailedMapLayer.set('name', 'japan_detailed_map');
+window.JapanDetailedMapLayer.set("name", "japan_detailed_map");
 
 window.JapanSimpleMapLayer = new ol.layer.Vector({
     source: JMAEQVectorSource,
-    style: defaultStyle
+    style: defaultStyle,
 });
 
-window.JapanSimpleMapLayer.set('name', 'japan_simple_map');
+window.JapanSimpleMapLayer.set("name", "japan_simple_map");
 
 const map = new ol.Map({
-    target: 'map',
+    target: "map",
     layers: [
         new ol.layer.Vector({
             source: ChinaMapVectorSource,
-            style: defaultStyle
+            style: defaultStyle,
         }),
         new ol.layer.Vector({
             source: NorthKoreaVectorSource,
-            style: defaultStyle
+            style: defaultStyle,
         }),
         new ol.layer.Vector({
             source: SouthKoreaVectorSource,
-            style: defaultStyle
+            style: defaultStyle,
         }),
         new ol.layer.Vector({
             source: TaiwanVectorSource,
             style: defaultStyle,
-        })
+        }),
     ],
     view: new ol.View({
         center: ol.proj.fromLonLat([140.412029876442, 37.3914691547508]), // initial value
-        zoom: 8
+        zoom: 8,
     }),
     controls: [],
 });
@@ -87,7 +87,8 @@ window.map = map;
 
 map.addLayer(JapanSimpleMapLayer);
 map.addLayer(JapanDetailedMapLayer);
-map.once('postrender', () => {
+map.once("postrender", () => {
     map.removeLayer(JapanSimpleMapLayer);
     map.removeLayer(JapanDetailedMapLayer);
 });
+
