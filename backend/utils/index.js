80f6697c77db2e906ec5448afbb5bb05d962ae18
Eridanus Sora <sora@sound.moe>
2022-05-20 22:14:03 +0800

    update deps

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,8 +1,4 @@
 class Utils {
-    static encodeParameters(parameters) {
-        const base64 = require("base64-utf8");
-        return base64.encode(JSON.stringify(parameters));
-    }
     static getTimestamp() {
         const time = new Date();
         return `${time.getFullYear()}-${
